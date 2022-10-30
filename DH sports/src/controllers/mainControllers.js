97070eda309d5d6f6c8fd713dd5a3f390a973011
nicocastiglioni <nicolascastiglioni@gmail.com>
2022-10-30 13:23:34 -0300

    rutas parametrizadas

--- DH sports/src/controllers/mainControllers.js
+++ DH sports/src/controllers/mainControllers.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
 const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
 
 let mainControllers = {
 
     index:(req,res) => {
-        res.render('index');
+        res.render('index', { products });
     },
 
     register:(req,res) => {
--- DH sports/src/controllers/productsControllers.js
+++ DH sports/src/controllers/productsControllers.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
 const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
 
 let productsControllers = {
     index: (req, res) => {
-        res.render('products/products')
+        res.render('products/products', {products})
     },
     detail: (req, res) => {
         res.render('products/productdetail')

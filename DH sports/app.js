462d6dcb141035569b9f079768ef84f2894ea609
nicocastiglioni <nicolascastiglioni@gmail.com>
2022-10-31 19:21:03 -0300

    agrego method-override

--- DH sports/app.js
+++ DH sports/app.js
@@ -3,13 +3,15 @@ const app = express();
 const path=require('path');
 const ejs = require('ejs');
 const mainRouter= require('./src/routes/mainRouter');
-const productsRouter = require('./src/routes/productsRouter')
-const usersRouter = require('./src/routes/usersRouter')
-const cartRouter = require('./src/routes/cartRouter')
+const productsRouter = require('./src/routes/productsRouter');
+const usersRouter = require('./src/routes/usersRouter');
+const cartRouter = require('./src/routes/cartRouter');
+const methodOverride = require('method-override');
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set('views',path.join(__dirname,'./src/views'));
-app.set('view engine','ejs')
+app.set('view engine','ejs');
+app.use(methodOverride('_method'));
 
 app.listen(3000, ()=>{
     console.log('Server corriendo en el puerto 3000');

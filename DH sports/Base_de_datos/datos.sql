9336888a96f32aad02a1ae6ff4a8ddbd999545e6
nicocastiglioni <nicolascastiglioni@gmail.com>
2023-02-13 18:02:30 -0300

    agrego algunos productos

--- DH sports/Base_de_datos/datos.sql
+++ DH sports/Base_de_datos/datos.sql
@@ -31,7 +31,10 @@ INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (2,'Nike');
 INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (3,'Everlast');
 INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (4,'Umbro');
 INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (5,'Puma');
-INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (6,'Otra');
+INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (6,'Topper');
+INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (7,'Under Armour');
+INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (8,'New Balance');
+INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (9,'Otra');
 
 
 /* CATEGORIES */
@@ -39,7 +42,13 @@ INSERT INTO `dhsports_db`.`brands` (`id`,`marca`) VALUES (6,'Otra');
 INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (1,'Camisetas');
 INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (2,'Botines');
 INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (3,'Guantes');
-INSERT INTO `dhsports_db`.`categories` (`id`, `categoria`) VALUES (4, 'Otras');
+INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (4,'Zapatillas');
+INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (5,'Medias');
+INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (6,'Shorts');
+INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (7,'Camperas');
+INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (8,'Remeras');
+INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (9,'Accesorios');
+INSERT INTO `dhsports_db`.`categories` (`id`,`categoria`) VALUES (10,'Otros');
 
 /* SPORTS */
 
@@ -47,7 +56,8 @@ INSERT INTO `dhsports_db`.`sports` (`id`, `sport`) VALUES (1, 'Básquet');
 INSERT INTO `dhsports_db`.`sports` (`id`, `sport`) VALUES (2, 'Fútbol');
 INSERT INTO `dhsports_db`.`sports` (`id`, `sport`) VALUES (3, 'Boxeo');
 INSERT INTO `dhsports_db`.`sports` (`id`, `sport`) VALUES (4, 'Hockey');
-INSERT INTO `dhsports_db`.`sports` (`id`, `sport`) VALUES (5, 'Otro');
+INSERT INTO `dhsports_db`.`sports` (`id`, `sport`) VALUES (5, 'Running');
+INSERT INTO `dhsports_db`.`sports` (`id`, `sport`) VALUES (6, 'Otro');
 
 
 /* COLORS */
@@ -71,8 +81,11 @@ INSERT INTO `dhsports_db`.`users` (`id`,`nombre`,`apellido`,`email`,`contraseña
 
 /* PRODUCTS */
 
-INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(1, 'camiseta Argentina', 20000, 10, 2, 1, 1, 5, 5, 1, 'camiseta campeon oficial 3 estrellas', 0,'Camiseta-Argentina.png');
-INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(2, 'botin preator', 30000, 20, 2, 1, 1, 19, 4, 2, 'botines preator azules', 0,'botines-rugby.jpg');
-INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(3, 'guantes everlast', 6000, 0, 3, 3, 3, 2, 1, 1, 'guentes de boxeo everlast', 0,'guantes-imagen3.webp');
-INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(4, 'palo de hockey', 15000, 10, 4, 1, 4, null, 3, 2, 'palo de Hockey para cesped', 0,'Hockey.jpg');
-INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(5, 'pelota al rihla', 10000, 0, 2, 1, 4, null, 5, 1, 'pelota oficial del mundial qatar 2022', 0,'pelota-imagen3.jpg');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(1, 'Camiseta Selección Argentina', 20000, 10, 2, 1, 1, 5, 5, 1, 'camiseta campeon oficial 3 estrellas', 0,'Camiseta-Argentina.png');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(2, 'Botín Preator', 30000, 20, 2, 1, 1, 19, 4, 2, 'botines preator azules', 0,'1676315071728.jpg');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(3, 'Guantes de Box Everlast', 6000, 0, 3, 3, 3, 2, 1, 1, 'guentes de boxeo everlast', 0,'guantes-imagen3.webp');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(4, 'Palo de Hockey', 15000, 10, 4, 1, 4, null, 3, 2, 'palo de Hockey para cesped', 0,'Hockey.jpg');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(5, 'Pelota Fútbol Al Rihla Adidas', 10000, 0, 2, 1, 4, null, 5, 1, 'pelota oficial del mundial qatar 2022', 0,'pelota-imagen3.jpg');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(6, 'Camiseta Básquet Chicago Bulls', 11150, 0, 1, 2, 1, 6, 1, 2, 'Camiseta de Lavine, de los Chichago Bulls', 0,'1675812852464.jpg');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(7, 'Zapatillas Running Adidas', 19900, 15, 5, 1, 4, 14, 4, 2, 'Zapatillas Running Adidas Solar Glide 5 Mujer', 0,'zapatillas-running.jpg');
+INSERT INTO `dhsports_db`.`products`(`id`,`nombre`,`precio`,`descuento`,`deporteId`,`marcaId`,`categoriaId`,`talleId`,`colorId`,`userId`,`descripcion`,`deleted`,`imagen`) VALUES(8, 'Campera Under Armour', 29900, 5, 5, 7, 7, 5, 2, 2, 'Campera Under Armour Anywhere Storm para hombre', 0,'Campera-UnderArmour.jpg');
